40c8a043fd9a0a2d471d272a700833711609861a
rubayatruba <74837296+rubayatruba@users.noreply.github.com>
2021-06-12 11:56:15 -0400

    Add files via upload

--- marblemaze_project.py
+++ marblemaze_project.py
@@ -1,8 +1,14 @@
-from sense_hat import SenseHat 
+from sense_emu import SenseHat 
+import anvil.server
+from anvil.tables import app_tables
+from sense_hat import SenseHat
 from time import sleep
+from datetime import datetime 
 
-
+# data = ""
 sense = SenseHat()
+
+anvil.server.connect("RAEJLWI3IW4QSTBYXRD5I63F-QXHXSHXFGW3EAOHT")
 
 sense.clear()
 
@@ -13,6 +19,36 @@ g = (0,255,0) #target green
 x = 1
 y = 1
 
+# def data():
+#     global data
+#     if maze[y][x] == r:
+#         sense.show_message("try again!")
+#         data = "lose"
+#         game_over = True
+#     elif maze[y][x] == g:
+#         sense.show_message("win!")
+#         data = "win"
+#         game_over = True
+        
+
+
+
+@anvil.server.callable
+def save_data():
+
+    # global data
+    data = "win"
+    dt=datetime.now()
+
+
+    new_row = app_tables.env_data1.add_row(datetime=dt,
+                                            message= data
+                                            )
+    
+    print(new_row)
+
+
+    
 
 maze = [[r, r, r, r, r, r, r, r],
         [r, b, b, b, b, b, b, r],
@@ -58,7 +94,16 @@ while game_over == False:
         game_over = True
     
 
+# while True:
+#     save_data()
+#     sleep(10)
 
+
+
+save_data()
+
+anvil.server.call('send_message','+16314340399', message= data )
+print("game over")
     
     
     
